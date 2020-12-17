07cc0e76a9ae675ee1df69153e9d313a26e7dc19
soongjamm <kimstz0303@gmail.com>
2020-12-18 00:36:20 +0900

    feat: 자동차 이름을 입력받고 ','를 기준으로 나눠서 리스트로 만듦

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -1,7 +1,20 @@
 package racingcar;
 
-public class Controller {
-    public void run() {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
+public class Controller {
+    private final Scanner scanner;
+
+    public Controller(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void run() {
+        InputView inputView = new InputView(scanner);
+        List<Car> cars = Arrays.stream(inputView.enterCars().split(","))
+                    .map(x -> new Car(x)).collect(Collectors.toList());
     }
 }
--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,8 +1,17 @@
 package racingcar;
 
-public class InputView {
-    public void enterCars() {
+import java.util.Scanner;
 
+public class InputView {
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String enterCars() {
+        System.out.println("자동차의 이름을 입력하세요.");
+        return scanner.nextLine().trim();
     }
 
     public void enterGameRound() {
