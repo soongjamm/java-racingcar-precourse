e637092974414a364625ca86f880f0c66ee9c09b
soongjamm <kimstz0303@gmail.com>
2020-12-18 00:44:24 +0900

    feat: 자동차 이름이 5자 이하인지와 공백이 아닌지 확인하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,15 +1,27 @@
 package racingcar;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
     }
 
-    // 추가 기능 구현
+    private void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(String.format("자동차 이름의 최대 길이는 %d를 넘을 수 없습니다.", MAX_NAME_LENGTH));
+        }
+        if (name.trim().equals("")) {
+            throw new IllegalArgumentException("자동차의 이름이 제대로 입력되지 않았습니다.");
+        }
+    }
+
     public void goForward() {
 
     }
+
+
 }
